5a3c57b42fa26cd4e2eb73ee72ff97b105305ab4
ferdlg <ferdlg@gmail.com>
2024-11-29 09:42:57 -0500

    add - testimonios de usuarios

--- bici-travesia/src/pages/Home.jsx
+++ bici-travesia/src/pages/Home.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import TestimonyCard from "../components/TestimonyCard"
 
 export const Home = () => {
   return (
+    <>
     <div>
       <h1>home</h1>
     </div>
+    <div>
+      <TestimonyCard></TestimonyCard>
+    </div>
+    </>
   )
 }
 
